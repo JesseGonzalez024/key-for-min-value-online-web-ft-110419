e3befd3652758941bc4eb528a6533fd0e02c9a92
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-09 23:24:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,11 @@
 def key_for_min_value(name_hash)
   small = nil
   small_name = nil
-  name_hash.select {|key, value|
+  name_hash.select do |key, value|
     if small == nil || small > value
-      
+      small = value
+      small_name = key
+    end
+  end
     
 end
