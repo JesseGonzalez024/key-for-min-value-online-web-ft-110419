d329f6a9ccca8990876376e185aac27920ac8f87
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-09 23:39:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   min = 0 
-  name_hash.each_value do |value|
+  name_hash.each do |key, value|
     if value < min
-      min = value
+      min = key
     end
   end
 end
